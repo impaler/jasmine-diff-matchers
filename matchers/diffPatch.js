6c59b9e950c870131615762f18154bd372ea6c22
benholloway <ben.holloway@3plearning.com>
2015-03-17 08:21:00 +1100

    refactor for jasmine 2

--- matchers/diffPatch.js
+++ matchers/diffPatch.js
@@ -4,65 +4,94 @@
  * @example expect('one two three').diffChars('one three two');
  *
  */
-var fs = require('fs'),
-    path = require('path');
+var fs       = require('fs'),
+    path     = require('path'),
+    isstring = require('lodash.isstring');
 
-var config = require('../config');
-var diffExpect = require('../diffUtil');
+var config     = require('../config'),
+    diffExpect = require('../diffUtil');
 
 module.exports = {
-    diffPatch: function (expected) {
-        if (expected === undefined) {
-            console.error('diffPatch() is missing a required parameter to compare to.');
+
+    /**
+     * Jasmine 2 matcher that performs difference on the given strings
+     * @returns {{compare: Function}}
+     */
+    diffPatch    : function () {
+        return {
+            compare: function (actual, expected) {
+                var pass;
+                var message = [actual, expected].map(function (value, i) {
+                        return !isstring(value) && ('value "' + (i ? 'expected' : 'actual') + '" must be a string');
+                    })
+                    .filter(Boolean)
+                    .join(', ');
+                if (message) {
+                    pass    = false;
+                } else {
+                    pass    = (actual === expected);
+                    message = diffExpect.diffPatch(this.actual, expected);
+                }
+                return {
+                    pass   : pass,
+                    message: message
+                };
+            }
         }
-
-        var result = (this.actual === expected);
-
-        if (!result) {
-            this.message = function () {
-                return diffExpect.diffPatch(this.actual, expected);
-            };
-        }
-
-        return result;
     },
-    diffFilePatch: function (expected) {
-        if (expected === undefined) {
-            console.error('diffFilePatch() is missing a required parameter to compare to.');
-        }
 
-        var actualFilePath = this.actual,
-            expectedFilePath = expected;
-
-        if(!fs.existsSync(actualFilePath)) {
-            actualFilePath = path.resolve(__dirname, '..', config.basePath, actualFilePath);
-
-            if(!fs.existsSync(actualFilePath)) {
-                console.error('diffFilePatch() the actualFilePath is does not exist', actualFilePath);
-                console.error('tried to used the config.basePath', config.basePath, 'with no success', actualFilePath);
+    /**
+     * Jasmine 2 matcher that performs file difference on the given file names
+     * @returns {{compare: Function}}
+     */
+    diffFilePatch: function () {
+        /**
+         * Attempt to load the given file path
+         * @param {string|Array.<string>} filePath The path to attempt load
+         * @returns {string|Buffer} The file buffer on success or a string on failure
+         */
+        function findFile(filePath) {
+            var joined  = [].concat(filePath)
+                .filter(isstring)
+                .join(path.sep);
+            var usePath  = [joined, [__dirname, '..', config.basePath, joined]]
+                .map(function resolve(elements, i) {
+                    return path.resolve.apply(path, [].concat(elements));
+                })
+                .filter(fs.existsSync)
+                .pop();
+            if (usePath) {
+                return fs.readFileSync(usePath);
+            } else {
+                return ['file does not exist', joined, '(tried to used the config.basePath', config.basePath,
+                    'with no success)'].join(' ');
             }
         }
-
-        if(!fs.existsSync(expectedFilePath)) {
-            expectedFilePath = path.resolve(__dirname, '..', config.basePath, expectedFilePath);
-
-            if(!fs.existsSync(expectedFilePath)) {
-                console.error('diffFilePatch() the expectedFilePath is does not exist', expectedFilePath);
-                console.error('tried to used the config.basePath', config.basePath, 'with no success', expectedFilePath);
+        function isFirstOccurance(value, i, array) {
+            return (array.indexOf(value) === i);
+        }
+        return {
+            compare: function (actual, expected) {
+                var actualFile   = findFile(actual),
+                    expectedFile = findFile(expected);
+                var pass;
+                var message = [actualFile,expectedFile]
+                    .filter(isstring)
+                    .filter(isFirstOccurance)
+                    .join(', ');
+                if (message) {
+                    pass    = false;
+                } else {
+                    var actualContent   = actualFile.toString();
+                    var expectedContent = expectedFile.toString();
+                    pass    = (actualContent === expectedContent);
+                    message = pass ? 'Files are matching' : diffExpect.diffPatch(actualContent, expectedContent);
+                }
+                return {
+                    pass   : pass,
+                    message: message
+                };
             }
         }
-
-        var actualContent = fs.readFileSync(String(actualFilePath), 'utf8') + '\n';
-        var expectedContent = fs.readFileSync(String(expectedFilePath), 'utf8') + '\n';
-
-        var result = (actualContent === expectedContent);
-
-        if (!result) {
-            this.message = function () {
-                return diffExpect.diffPatch(actualContent, expectedContent);
-            };
-        }
-
-        return result;
     }
 };
